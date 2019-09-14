cba24e8117f8f684136600d656419453afa50d0a
VenomRishi <mhatrerishikesh@gmail.com>
2019-09-14 19:20:46 +0530

    minor changes done

--- src/com/bridgelabz/dataStructure/P13PrimeAnagramUsingStack.java
+++ src/com/bridgelabz/dataStructure/P13PrimeAnagramUsingStack.java
@@ -17,39 +17,36 @@ public class P13PrimeAnagramUsingStack {
 
 	public static void main(String[] args) {
 		int start = 0;
-		int end = 100;
+		int end = 1000;
 
 		StackImplementUsingLinkedList<Integer> stack = new StackImplementUsingLinkedList<Integer>();
 
 		// putting values in array
-		for (int i = 0; i < 10; i++) {
-			for (int j = start; j <= end; j++) {
-				if (DataStructureUtility.isPrime(j)) {
-					for (int k = j + 1; k < end; k++) {
-						if (DataStructureUtility.isPrime(k)) {
-							if (DataStructureUtility.isAnagram(String.valueOf(j), String.valueOf(k))) {
-								stack.push(j);
-								stack.push(k);
 
-							}
+		for (int j = start; j <= end; j++) {
+			if (DataStructureUtility.isPrime(j)) {
+				for (int k = j + 1; k < end; k++) {
+					if (DataStructureUtility.isPrime(k)) {
+						if (DataStructureUtility.isAnagram(String.valueOf(j), String.valueOf(k))) {
+							stack.push(j);
+							stack.push(k);
+
 						}
 					}
-
 				}
+
 			}
-			start = end + 1;
-			end += 100;
+
 		}
-		//stack.show();
-		
-		int tempSize=stack.size();
+		// stack.show();
+
+		int tempSize = stack.size();
 		for (int i = 0; i < tempSize; i++) {
-			System.out.print(stack.peek()+" ");
+			System.out.print(stack.peek() + " ");
 			if (!stack.isEmpty()) {
 				stack.pop();
 			}
 		}
-		
 
 	}
 
--- src/com/bridgelabz/dataStructure/P14PrimeAnagramUsingQueue.java
+++ src/com/bridgelabz/dataStructure/P14PrimeAnagramUsingQueue.java
@@ -17,43 +17,41 @@ public class P14PrimeAnagramUsingQueue {
 
 	public static void main(String[] args) {
 		int start = 0;
-		int end = 100;
+		int end = 1000;
 		QueueImplementedUsingLinkedList<Integer> queue = new QueueImplementedUsingLinkedList<Integer>();
 		// putting values in array
-		for (int i = 0; i < 10; i++) {
-			for (int j = start; j <= end; j++) {
-				if (DataStructureUtility.isPrime(j)) {
-					for (int k = j + 1; k < end; k++) {
-						if (DataStructureUtility.isPrime(k)) {
-							if (DataStructureUtility.isAnagram(String.valueOf(j), String.valueOf(k))) {
-								queue.enQueue(j);
-								queue.enQueue(k);
 
-							}
+		for (int j = start; j <= end; j++) {
+			if (DataStructureUtility.isPrime(j)) {
+				for (int k = j + 1; k < end; k++) {
+					if (DataStructureUtility.isPrime(k)) {
+						if (DataStructureUtility.isAnagram(String.valueOf(j), String.valueOf(k))) {
+							queue.enQueue(j);
+							queue.enQueue(k);
+
 						}
 					}
-
 				}
+
 			}
-			start = end + 1;
-			end += 100;
+
 		}
 
 		int tempSize = queue.size();
 		for (int i = 0; i < tempSize; i++) {
 			System.out.print(queue.get(i) + " ");
-			if (queue.get(i) > 90 && queue.get(i) < 105)
-				System.out.println();
-			if (queue.get(i) > 190 && queue.get(i) < 205)
-				System.out.println();
-			if (queue.get(i) > 285 && queue.get(i) < 305)
-				System.out.println();
-			if (queue.get(i) > 390 && queue.get(i) < 405)
-				System.out.println();
-			if (queue.get(i) > 490 && queue.get(i) < 505)
-				System.out.println();
-			if (queue.get(i) > 690 && queue.get(i) < 705)
-				System.out.println();
+//			if (queue.get(i) > 90 && queue.get(i) < 105)
+//				System.out.println();
+//			if (queue.get(i) > 190 && queue.get(i) < 205)
+//				System.out.println();
+//			if (queue.get(i) > 285 && queue.get(i) < 305)
+//				System.out.println();
+//			if (queue.get(i) > 390 && queue.get(i) < 405)
+//				System.out.println();
+//			if (queue.get(i) > 490 && queue.get(i) < 505)
+//				System.out.println();
+//			if (queue.get(i) > 690 && queue.get(i) < 705)
+//				System.out.println();
 //				queue.deQueue();
 
 		}
